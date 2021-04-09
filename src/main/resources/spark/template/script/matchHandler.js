7e762629f518cbc2dbd9a0846fbb883e3dd011b3
your_username <delorazli@gmail.com>
2021-04-09 16:34:17 -0400

    script setup almost done

--- src/main/resources/spark/template/script/matchHandler.js
+++ src/main/resources/spark/template/script/matchHandler.js
@@ -3,27 +3,21 @@ const matchGrade = document.getElementById('match-grade');
 const matchMajor = document.getElementById('match-major');
 const matchInterests = document.getElementById("top_interests_list");
 
-fetch( '/yoki', {
-    method: 'post',
-    body: JSON.stringify(postParameters),
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-    },
-})
-
-    .then((response) =>
-        response.json())
-    .then((data) => {
-        for (i=0; i<data.message.length; i++) {
-            let msg = data.message[i];
-            suggestionList.innerHTML += "<li tabindex=\"0\">" + msg + "<\li>";
-        }
-        let listItems = document.querySelectorAll("li");
-        for (let i = 0; i < listItems.length; i++) {
-            listItems[i].addEventListener("click", () => {
-                let msg = data.message[i];
-                input.value = msg;
-                loading.style.display = "";
-            });
-        }
-    });
+const getNextMatch = () => {
+    fetch('/yoki', {
+        method: 'post',
+        body: JSON.stringify(postParameters),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then(response => {
+            console.log(response.data.name)
+            matchName.innerHTML = response.data.name;
+            matchGrade.innerHTML = "Class of " + response.data.grade;
+            matchMajor.innerHTML = response.data.major;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
