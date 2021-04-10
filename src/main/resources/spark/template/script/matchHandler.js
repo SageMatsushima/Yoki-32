03345e2194cdc613bf0c40dc43c95577edf372a7
your_username <delorazli@gmail.com>
2021-04-09 22:23:40 -0400

    added js for matching - first go

--- src/main/resources/spark/template/script/matchHandler.js
+++ src/main/resources/spark/template/script/matchHandler.js
@@ -2,8 +2,17 @@ const matchName = document.getElementById('match-name');
 const matchGrade = document.getElementById('match-grade');
 const matchMajor = document.getElementById('match-major');
 const matchInterests = document.getElementById("top_interests_list");
+const matchList = document.getElementById('matchList');
+const matchMap = new Map();
 
-const getNextMatch = () => {
+
+function onMatchPressed() {
+    let response = getNextMatch();
+    matchMap.append(response.data.name, response);
+}
+
+
+function getNextMatch(){
     fetch('/yoki', {
         method: 'post',
         body: JSON.stringify(postParameters),
@@ -16,6 +25,7 @@ const getNextMatch = () => {
             matchName.innerHTML = response.data.name;
             matchGrade.innerHTML = "Class of " + response.data.grade;
             matchMajor.innerHTML = response.data.major;
+            return response.data;
         })
         .catch(function (error) {
             console.log(error);
