90c4f5b97fb7e479dc0cf5aea89762a3d28c71e4
SageMatsushima <sage_matsushima@brown.edu>
2021-04-13 12:07:44 -0700

    pull

--- target/classes/static/script/interestButtons.js
+++ target/classes/static/script/interestButtons.js
@@ -43,5 +43,41 @@ function allInterests() {
         .catch(function (error) {
             console.log(error);
         });
+}
 
+function onSavePressed(value) {
+    save();
+    matchMap.set(currUser.firstName, currUser);
+}
+
+function save() {
+    fetch('http://localhost:4567/listInterests', {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) =>
+            response.json())
+        .then((data) => {
+            let matchMap = new Map();
+            Object.keys(matchMap).map(function(key) {
+                matchList.innerHTML = "<li> " + matchMap[key].firstName + " </li>";
+            });
+            for (const [key, value] of Object.entries(data.interestsList)) {
+                console.log(`${key}: ${value.name}`);
+                const interest = document.createElement("button");
+                interest.addEventListener("click", () => addInterestDiv(value));
+                interest.className = "subject";
+                interest.id = key;
+                interest.innerHTML = value.name;
+                document.getElementById("subjects").appendChild(interest);
+            }
+
+            //matchMajor.innerHTML = response.data.major;
+            return data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
