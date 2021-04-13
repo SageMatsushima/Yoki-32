8ac62b43ba94ba2ae066e9db57f99c0414de0848
Ian Acosta <ianacosta@Ians-MacBook-Pro.local>
2021-04-13 13:31:28 -0700

    matches update db

--- target/classes/static/script/matchUpdate.js
+++ target/classes/static/script/matchUpdate.js
@@ -1,33 +1,16 @@
-const matchName = document.getElementById('match-name');
-const matchGrade = document.getElementById('match-grade');
-const matchMajor = document.getElementById('match-major');
-const matchInterests = document.getElementById("top_interests_list");
-const matchList = document.getElementById('matchList');
-const matchMap = new Map();
+let matchSet = new Set();
+
+function addMatchDiv(matched) {
+    const match = document.createElement("div");
+    match.className = "match";
+    match.id = "matchCard";
+    const name = document.createElement("h3");
+    name.innerHTML = matched.firstName;
 
 
-function onMatchPressed() {
-    let response = getNextMatch();
-    matchMap.append(response.data.name, response);
+    document.getElementById("match-list").appendChild(match);
 }
 
+function addMatches() {
 
-function getNextMatch(){
-    fetch('/yoki', {
-        method: 'post',
-        body: JSON.stringify(postParameters),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then(response => {
-            console.log(response.data.name)
-            matchName.innerHTML = response.data.name;
-            matchGrade.innerHTML = "Class of " + response.data.grade;
-            matchMajor.innerHTML = response.data.major;
-            return response.data;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
 }
