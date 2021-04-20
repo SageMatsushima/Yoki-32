ddd96aa26ffcecb530f1b55cc129b797acb4a5ad
Ian Acosta <ianacosta@Ians-MacBook-Pro.local>
2021-04-20 12:30:15 -0700

    pull imgs

--- src/main/resources/static/script/settings.js
+++ src/main/resources/static/script/settings.js
@@ -33,13 +33,21 @@ function login() {
 }
 
 function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+    document.documentElement.style.setProperty('--first', "#1d2d50");
+    document.documentElement.style.setProperty('--second', "#1d2d50");
+    document.documentElement.style.setProperty('--third', "#133b5c");
+    document.documentElement.style.setProperty('--fourth', "#1e5f74");
+    document.documentElement.style.setProperty('--fifth', "#fcdab7");
+    document.documentElement.style.setProperty('--sixth', "#dbdbdb");
 }
 
 function lightMode () {
-    var element = document.body;
-    element.classList.toggle("light-mode");
+    document.documentElement.style.setProperty('--first', "white");
+    document.documentElement.style.setProperty('--second', "#dbdbdb");
+    document.documentElement.style.setProperty('--third', "#fff9ef");
+    document.documentElement.style.setProperty('--fourth', "#ffe0a4");
+    document.documentElement.style.setProperty('--fifth', "#FFB84F");
+    document.documentElement.style.setProperty('--sixth', "#804C00");
 }
 
 function lightDark() {
